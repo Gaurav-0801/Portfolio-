97a8fa34ac95451576708a15c95ec6610ce77ca4
Gaurav-0801 <gaurav.pant2021@vitbhopal.ac.in>
2025-07-31 21:54:28 +0530

    init

---
+++ src/constants/index.js
@@ -0,0 +1,300 @@
+export const myProjects = [
+{
+  id: 1,
+  title: "Bolt.new",
+  description:
+    "A no-code website builder that generates fully functional websites from user prompts using AI, providing instant live previews and editable code.",
+  subDescription: [
+    "Developed a full-stack platform using Next.js, React, and Tailwind CSS for dynamic UI and real-time feedback.",
+    "Integrated OpenAI’s API to convert natural language prompts into structured, editable HTML/CSS/JS code files.",
+    "Implemented a live preview system using WebContainers to run and display code directly in-browser.",
+    "Enabled seamless project creation, editing, and sharing without requiring any programming knowledge from users.",
+  ],
+  href: "https://bolt-new-x2v6.vercel.app/",
+  logo: "",
+  image: "/assets/projects/bolt-builder.jpg",
+  tags: [
+    {
+      id: 1,
+      name: "Next.js",
+      path: "/assets/logos/nextjs.svg",
+    },
+    {
+      id: 2,
+      name: "TypeScript",
+      path: "/assets/logos/typescript.svg",
+    },
+    {
+      id: 3,
+      name: "React",
+      path: "/assets/logos/react.svg",
+    },
+    {
+      id: 4,
+      name: "TailwindCSS",
+      path: "/assets/logos/tailwindcss.svg",
+    },
+    {
+      id: 5,
+      name: "OpenAi",
+      path: "/assets/logos/chatgpt.svg",
+    },
+  ],
+}
+,
+{
+  id: 2,
+  title: "Podcastr - AI-Powered Podcast Platform",
+  description:
+    "An AI-integrated podcast platform that allows users to upload, convert text to human speech, and generate thumbnails automatically.",
+  subDescription: [
+    "Built text-to-speech functionality using advanced AI models to convert any text into 5 different human voices.",
+    "Integrated thumbnail generation based on podcast content using OpenAI image models.",
+    "Developed the frontend using Next.js and Tailwind CSS for a smooth and responsive UI.",
+    "Used Convex as the backend for real-time database and serverless function support.",
+  ],
+  href: "https://www.podcastrr.me/",
+  logo: "",
+  image: "/assets/projects/podcastr.jpg",
+  tags: [
+    {
+      id: 1,
+      name: "Next.js",
+      path: "/assets/logos/nextjs.svg",
+    },
+    {
+      id: 2,
+      name: "TailwindCSS",
+      path: "/assets/logos/tailwindcss.svg",
+    },
+    {
+      id: 3,
+      name: "OpenAi",
+      path: "/assets/logos/chatgpt.svg",
+    },
+    {
+      id: 4,
+      name: "Convex",
+      path: "/assets/logos/convex.svg",
+    },
+  ],
+},
+
+{
+  id: 3,
+  title: "Brainly Application",
+  description:
+    "A digital second brain web app to save, filter, and share YouTube videos and tweets effortlessly.",
+  subDescription: [
+    "Implemented JWT-based authentication for secure user access and session management.",
+    "Enabled category-based filtering and content saving for organized knowledge storage.",
+    "Used React with TypeScript and Vite for a fast, modular, and responsive frontend.",
+    "Built RESTful APIs with Node.js and Express, and used MongoDB for data persistence.",
+  ],
+  href: "https://brainly-neon-xi.vercel.app",
+  logo: "",
+  image: "/assets/projects/brainly-app.jpg",
+  tags: [
+    {
+      id: 1,
+      name: "React",
+      path: "/assets/logos/react.svg",
+    },
+    {
+      id: 2,
+      name: "TypeScript",
+      path: "/assets/logos/typescript.svg",
+    },
+    {
+      id: 3,
+      name: "MongoDB",
+      path: "/assets/logos/mongodb.svg",
+    },
+    {
+      id: 4,
+      name: "Node.js",
+      path: "/assets/logos/nodejs.svg",
+    },
+  ],
+},
+
+ {
+  id: 4,
+  title: "Inventory Management App",
+  description:
+    "A full-stack inventory management system designed to streamline tracking, purchasing, and stock control for small to mid-sized businesses.",
+  subDescription: [
+    "Built using Next.js,Node.js, Express, and Prisma for scalable server-side operations and efficient database management.",
+    "Designed a modern, responsive frontend with React and Tailwind CSS for a smooth user experience.",
+    "Integrated real-time tracking of inventory, suppliers, purchases, and sales using PostgreSQL.",
+    "Added features like  purchase history and analytics dashboards for smarter inventory control.",
+  ],
+  href: "https://todo-app-eight-blue-28.vercel.app",
+  logo: "",
+  image: "/assets/projects/inventory-app.jpg", 
+  tags: [
+    {
+      id: 1,
+      name: "Node.js",
+      path: "/assets/logos/nodejs.svg",
+    },
+    {
+      id: 2,
+      name: "React",
+      path: "/assets/logos/react.svg",
+    },
+    {
+      id: 3,
+      name: "Prisma",
+      path: "/assets/logos/prisma.svg",
+    },
+    {
+      id: 4,
+      name: "TailwindCSS",
+      path: "/assets/logos/tailwindcss.svg",
+    },
+    {
+      id: 5,
+      name: "PostgreSQL",
+      path: "/assets/logos/postgresql.svg",
+    },
+  ],
+}
+,
+  {
+    id: 5,
+    title: "Full Stack ToDo App",
+    description:
+      "A task management web app with authentication, CRUD operations, and persistent database storage.",
+    subDescription: [
+      "Built using React for the frontend and Node.js with Express for the backend.",
+      "Integrated Prisma ORM with PostgreSQL for reliable database management.",
+      "Implemented JWT-based authentication and protected API routes.",
+      "Used Tailwind CSS for responsive design and modern UI components.",
+    ],
+    href: "https://todo-app-eight-blue-28.vercel.app",  
+    logo: "", 
+    image: "/assets/projects/todo-app.jpg", 
+    tags: [
+      {
+        id: 1,
+        name: "React",
+        path: "/assets/logos/react.svg",
+      },
+      {
+        id: 2,
+        name: "Node.js",
+        path: "/assets/logos/nodejs.svg",
+      },
+      {
+        id: 3,
+        name: "PostgreSQL",
+        path: "/assets/logos/postgresql.svg",
+      },
+      {
+        id: 4,
+        name: "Prisma",
+        path: "/assets/logos/prisma.svg",
+      },
+    ],
+  }, 
+]; 
+
+export const mySocials = [
+  {
+    name: "WhatsApp",
+    href: "",
+    icon: "/assets/socials/whatsApp.svg",
+  },
+  {
+    name: "Linkedin",
+    href: "https://www.linkedin.com/in/gaurav-pant-74b9ab265/",
+    icon: "/assets/socials/linkedIn.svg",
+  },
+  {
+    name: "Instagram",
+    href: "https://www.instagram.com/gaurav_0801?igsh=eXdmdGt1amw0NHho",
+    icon: "/assets/socials/instagram.svg",
+  },
+];
+
+export const experiences = [
+  {
+    title: "The Complete 2024 Web Development Bootcamp",
+    issuer: "Udemy",
+    date: "2024",
+    contents: [
+      "Comprehensive course covering HTML, CSS, JavaScript, Node.js, React, MongoDB, and more.",
+      "Hands-on projects including blog sites, authentication systems, and RESTful APIs.",
+    ],
+  },
+  {
+    title: "Java Fullstack Internship Certification",
+    issuer: "Internship Program",
+    date: "Aug 2022 – Sep 2022",
+    contents: [
+      "Covered Java programming, front-end development using HTML and CSS, and MySQL database integration.",
+      "Built full-stack applications with user interfaces and backend logic.",
+    ],
+  },
+  {
+    title: "MERN Fullstack Internship Certification",
+    issuer: "Internship Program",
+    date: "Aug 2022 – Sep 2022",
+    contents: [
+      "Focused on building applications using MongoDB, Express.js, React.js, and Node.js.",
+      "Learned responsive design with Bootstrap and API development.",
+    ],
+  },
+];
+
+export const reviews = [
+  {
+    name: "Jack",
+    username: "@jack",
+    body: "I've never seen anything like this before. It's amazing. I love it.",
+    img: "https://robohash.org/jack",
+  },
+  {
+    name: "Jill",
+    username: "@jill",
+    body: "I don't know what to say. I'm speechless. This is amazing.",
+    img: "https://robohash.org/jill",
+  },
+  {
+    name: "John",
+    username: "@john",
+    body: "I'm at a loss for words. This is amazing. I love it.",
+    img: "https://robohash.org/john",
+  },
+  {
+    name: "Alice",
+    username: "@alice",
+    body: "This is hands down the best thing I've experienced. Highly recommend!",
+    img: "https://robohash.org/alice",
+  },
+  {
+    name: "Bob",
+    username: "@bob",
+    body: "Incredible work! The attention to detail is phenomenal.",
+    img: "https://robohash.org/bob",
+  },
+  {
+    name: "Charlie",
+    username: "@charlie",
+    body: "This exceeded all my expectations. Absolutely stunning!",
+    img: "https://robohash.org/charlie",
+  },
+  {
+    name: "Dave",
+    username: "@dave",
+    body: "Simply breathtaking. The best decision I've made in a while.",
+    img: "https://robohash.org/dave",
+  },
+  {
+    name: "Eve",
+    username: "@eve",
+    body: "So glad I found this. It has changed the game for me.",
+    img: "https://robohash.org/eve",
+  },
+];
